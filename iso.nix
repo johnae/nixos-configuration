a3e1d77cfa65e9dfb2ed0de24a2acdaa7693e784
John Axel Eriksson <john@insane.se>
2020-01-12 21:06:05 +0100

    Fix iso build

--- iso.nix
+++ iso.nix
@@ -16,8 +16,8 @@ let
 in
 {
   imports = [
-    "<nixpkgs>/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-    "<nixpkgs>/nixos/modules/installer/cd-dvd/channel.nix")
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
   environment.etc = {
