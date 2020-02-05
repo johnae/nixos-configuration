2f3ded092495e263cf5a06335c2f30285b76f32e
John Axel Eriksson <john@insane.se>
2020-02-05 13:57:38 +0100

    i3-status cleanup

--- home/i3-status.nix
+++ home/i3-status.nix
@@ -1,7 +1,7 @@
 { pkgs, config, lib, options }:
 
 let
-  check-nixos-version = pkgs.writeStrictShellScriptBin "check-nixos-version" ''
+  checkNixosVersion = pkgs.writeStrictShellScriptBin "check-nixos-version" ''
     CURRENT=$(${pkgs.curl}/bin/curl -sS https://howoldis.herokuapp.com/api/channels | \
               ${pkgs.jq}/bin/jq -r '.[] | select(.name == "nixos-unstable") | "\(.link) \(.time)"')
     AGE_SECS=$(echo "$CURRENT" | awk '{print $2}')
@@ -25,13 +25,12 @@ in
 {
   programs.i3status-rust = {
      enable = true;
-     settings = {
-       block = [
+     settings.block = [
 
          {
            block = "custom";
            interval = 600;
-           command = "${check-nixos-version}/bin/check-nixos-version";
+           command = "${checkNixosVersion}/bin/check-nixos-version";
          }
 
          {
@@ -80,55 +79,4 @@ in
 
        ];
      };
-     #blocks = {
-     #  custom = {
-     #    opts = {
-     #      interval = 600;
-     #      command = "${check-nixos-version}/bin/check-nixos-version";
-     #    };
-     #  };
-     #  cpu = {
-     #    opts.interval = 1;
-     #  };
-     #  backlight = {
-     #  };
-     #  battery = {
-     #    opts = {
-     #      interval = 10;
-     #      format = "{percentage}% {time}";
-     #    };
-     #  };
-     #  net = {
-     #    opts = {
-     #      device = "wlan0";
-     #      ssid = true;
-     #      signal_strength = true;
-     #      ip = false;
-     #      speed_up = true;
-     #      graph_up = false;
-     #      interval = 5;
-     #    };
-     #  };
-
-     #  sound = {
-     #  };
-
-     #  #headphones = {
-     #  #  block = "bluetooth";
-     #  #  opts.mac = "04:52:C7:5F:CC:B6";
-     #  #};
-
-     #  #mouse = {
-     #  #  block = "bluetooth";
-     #  #  opts.mac = "D5:17:1A:80:22:AA";
-     #  #};
-
-     #  time = {
-     #    opts = {
-     #      interval = 1;
-     #      format = "%b-%d %H:%M:%S";
-     #    };
-     #  };
-     #};
-  };
 }
