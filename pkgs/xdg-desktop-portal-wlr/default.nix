3dddbe1468e3081249431df945d80058a225da04
John Axel Eriksson <john@insane.se>
2020-02-18 16:18:13 +0100

    Remove some junk

--- pkgs/xdg-desktop-portal-wlr/default.nix
+++ pkgs/xdg-desktop-portal-wlr/default.nix
@@ -1,7 +1,3 @@
-# { stdenv, fetchFromGitHub, meson, ninja, pkgconfig, wayland, scdoc, makeWrapper
-#, wayland-protocols, gdk_pixbuf, dbus_libs, pango, cairo, git, systemd, librsvg
-#}:
-
 { stdenv
 , fetchFromGitHub
 , meson
@@ -30,15 +26,6 @@ stdenv.mkDerivation rec {
   nativeBuildInputs = [ meson ninja pkgconfig git scdoc makeWrapper ];
 
   buildInputs = [ cairo pango wayland wayland-protocols systemd gdk_pixbuf ];
-  #buildInputs = [
-  #  wayland wayland-protocols dbus_libs
-  #  pango cairo systemd gdk_pixbuf librsvg
-  #];
-
-  #postInstall = ''
-  #  wrapProgram $out/bin/mako \
-  #   --set GDK_PIXBUF_MODULE_FILE "${librsvg}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache"
-  #'';
 
   meta = with stdenv.lib; {
     description = "screen sharing for wayland";
