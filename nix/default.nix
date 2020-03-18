549e0af11bb1f4753d780a4d9256872dd32eb5c0
John Axel Eriksson <john@insane.se>
2020-03-18 19:31:30 +0100

    Add nix/default.nix

--- nix/default.nix
+++ nix/default.nix
@@ -1,3 +1,9 @@
-(import ./nixpkgs.nix) {
-  overlays = (import ./nixpkgs-overlays.nix);
-}
+{ system ? null, config ? { }, ... }:
+
+import ./nixpkgs.nix (
+  {
+    overlays = (import ./nixpkgs-overlays.nix);
+  }
+  // (if system != null then { inherit system; } else { })
+  // { inherit config; }
+)
