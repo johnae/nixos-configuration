36608b12e0cf00f0a7b9752b60cd354ae6588349
John Axel Eriksson <john@insane.se>
2020-04-12 17:18:37 +0200

    Remove local mako module, use new one in home-manager

--- home/mako.nix
+++ home/mako.nix
@@ -3,21 +3,19 @@
 rec {
   programs.mako = {
     enable = true;
-    settings = rec {
-      font = "Roboto";
-      background-color = "#000021DD";
-      text-color = "#FFFFFFFF";
-      border-size = "0";
-      border-radius = "15";
-      icons = "1";
-      icon-path = "${pkgs.moka-icon-theme}/share/icons/Moka";
-      markup = "1";
-      actions = "1";
-      default-timeout = "3000"; ## ms
-      padding = "20";
-      height = "200";
-      width = "500";
-      layer = "overlay";
-    };
+    font = "Roboto";
+    backgroundColor = "#000021DD";
+    textColor = "#FFFFFFFF";
+    borderSize = 0;
+    borderRadius = 15;
+    icons = true;
+    iconPath = "${pkgs.moka-icon-theme}/share/icons/Moka";
+    markup = true;
+    actions = true;
+    defaultTimeout = 3000;
+    padding = "20";
+    height = 200;
+    width = 500;
+    layer = "overlay";
   };
 }
