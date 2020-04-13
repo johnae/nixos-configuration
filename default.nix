378069210cda1237905a8a3989f9b30d9fcef8d1
John Axel Eriksson <john@insane.se>
2020-04-13 22:14:47 +0200

    Build gnupg temporarily

--- default.nix
+++ default.nix
@@ -91,7 +91,7 @@ rec {
           (builtins.readDir ./pkgs)
       );
     in with pkgs;
-    pkgs.recurseIntoAttrs toCache;
+    pkgs.recurseIntoAttrs (toCache // { inherit gnupg; });
   installers = {
     europa = buildIso ./machines/europa/configuration.nix;
     phobos = buildIso ./machines/phobos/configuration.nix;
