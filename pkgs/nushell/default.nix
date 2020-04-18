943c16e9c8eca43f589ac322a9cc189878e84096
buildkite-agent-linux-1 <buildkite@buildkite-agent-linux-1>
2020-04-18 06:28:26 +0000

    Auto updated nushell

--- pkgs/nushell/default.nix
+++ pkgs/nushell/default.nix
@@ -19,7 +19,7 @@ rustPlatform.buildRustPackage rec {
 
   src = fetchFromGitHub metadata;
 
-  cargoSha256 = "10igih5mblilamwy1inzl0bwhbhc2q2vbqg9f2n4q0irdbwa30j3";
+  cargoSha256 = "1n2mjb7zbnnx02fd7bmykyy0ggr8cwp8jv5934x39w0xwb20v5vf";
 
   nativeBuildInputs = [ pkg-config python3 ];
 
