0665dcf025b141cbf830db16115d5f62c9f7a92f
buildkite-agent-linux-2 <buildkite@buildkite-agent-linux-2>
2020-04-29 00:23:08 +0000

    Auto updated i3status-rust

--- pkgs/i3status-rust/default.nix
+++ pkgs/i3status-rust/default.nix
@@ -7,7 +7,7 @@ rustPlatform.buildRustPackage rec {
   version = metadata.rev;
 
   src = fetchFromGitHub metadata;
-  cargoSha256 = "1bc6fklv1xklssk4kidfaj70j2kag3rfanqiz9k5k41qzf314bqr";
+  cargoSha256 = "00j4znvf6h91mxn23kxygxich0pm2jrdbgqd893szhh6f42nmvvs";
 
   nativeBuildInputs = [ pkgconfig ];
 
