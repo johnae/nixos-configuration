d25579d673a4e1c840145d5be463d2e1b94cb363
John Axel Eriksson <john@insane.se>
2020-05-09 09:55:10 +0200

    formatting

--- nix/nixpkgs-config.nix
+++ nix/nixpkgs-config.nix
@@ -1,7 +1,13 @@
 {
   allowUnfree = true;
   nix = {
-    binaryCachePublicKeys = [ "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA=" "insane.cachix.org-1:cLCCoYQKkmEb/M88UIssfg2FiSDUL4PUjYj9tdo4P8o=" ];
-    binaryCaches = [ "https://nixpkgs-wayland.cachix.org" "https://insane.cachix.org" ];
+    binaryCachePublicKeys = [
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "insane.cachix.org-1:cLCCoYQKkmEb/M88UIssfg2FiSDUL4PUjYj9tdo4P8o="
+    ];
+    binaryCaches = [
+      "https://nixpkgs-wayland.cachix.org"
+      "https://insane.cachix.org"
+    ];
   };
 }
