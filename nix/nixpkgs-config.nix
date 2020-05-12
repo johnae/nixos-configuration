92e3cbc102222869d523fc386f7839b37b98b913
John Axel Eriksson <john@insane.se>
2020-05-12 09:03:06 +0200

    Add cache.nixos.org to nixpkgs-config

--- nix/nixpkgs-config.nix
+++ nix/nixpkgs-config.nix
@@ -2,10 +2,12 @@
   allowUnfree = true;
   nix = {
     binaryCachePublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "insane.cachix.org-1:cLCCoYQKkmEb/M88UIssfg2FiSDUL4PUjYj9tdo4P8o="
     ];
     binaryCaches = [
+      "https://cache.nixos.org"
       "https://nixpkgs-wayland.cachix.org"
       "https://insane.cachix.org"
     ];
