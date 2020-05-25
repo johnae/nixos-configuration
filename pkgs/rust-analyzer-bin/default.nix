ced3eb1d39bca8613b0e5ccfe0bb98831b2ce4bd
John Axel Eriksson <john@insane.se>
2020-05-25 20:23:36 +0200

    rust-analyzer: use patchelf to fix dynamically linked binary

--- pkgs/rust-analyzer-bin/default.nix
+++ pkgs/rust-analyzer-bin/default.nix
@@ -9,6 +9,11 @@ stdenv.mkDerivation rec {
     chmod +x $out/bin/rust-analyzer
     ln -s $out/bin/rust-analyzer $out/bin/ra_lsp_server
   '';
+  fixupPhase = ''
+    chmod 755 $out/bin/rust-analyzer
+    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+             $out/bin/rust-analyzer
+  '';
 
   unpackPhase = "true";
   buildPhase = "true";
