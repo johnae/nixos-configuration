0a18ecf1a1626791f69de349530a536bd6c2f646
John Axel Eriksson <john@insane.se>
2020-07-02 09:05:45 +0200

    Auto updated spotnix cargo dependencies

--- pkgs/spotnix/default.nix
+++ pkgs/spotnix/default.nix
@@ -5,7 +5,7 @@ rustPlatform.buildRustPackage rec {
   version = sources.spotnix.rev;
 
   src = sources.spotnix;
-  cargoSha256 = "0p0jvd2f0x5hx1lzk83x00ayvg7ac127cdxnv7mzhr9qpvk6059s";
+  cargoSha256 = "0000000000000000000000000000000000000000000000000000";
 
   nativeBuildInputs = [ pkgconfig ];
 
